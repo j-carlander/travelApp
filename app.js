0a2fe7c1d76804dd0da25eec3a86700cf8a4e647
j-carlander <johannes.carlander@gmail.com>
2023-01-13 10:44:22 +0100

    added a search button to be able to search from mobile

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let renderTrainTable;
+let renderTrainTable; // declaring, will have a function asigned in the if(mobile)else(desktop)
 
 if (window.matchMedia("(max-width: 600px)").matches) {
   // The viewport is less than 440 pixels wide
@@ -8,33 +8,33 @@ if (window.matchMedia("(max-width: 600px)").matches) {
     data.forEach((trainObject) => {
       let element = $(`
       <details class="main-details">
-      <summary>
-      <div class="summary-title">
-        <h3>${trainObject.departTime} &#10140; ${trainObject.arrTime}</h3>
-        <p>${trainObject.name}</p>
-      </div>
-      <div class="summary-chevron-up">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-      </div>
-      </summary>
-      <div class="summary-content">
-        <p>${trainObject.origin} &#10140; ${trainObject.dest}</p>
-        <details class="sub-details">
-          <summary>
-            <span class="summary-title">Information och Service ombord</span>
-            <div class="summary-chevron-up">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-            </div>
-          </summary>
-          <ul class="content-list"></ul>
-          <div class="summary-chevron-down">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+        <summary>
+          <div class="summary-title">
+            <h3>${trainObject.departTime} &#10140; ${trainObject.arrTime}</h3>
+            <p>${trainObject.name}</p>
           </div>
-        </details>
-      </div>
-      <div class="summary-chevron-down">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
-      </div>
+          <div class="summary-chevron-up">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+          </div>
+        </summary>
+        <div class="summary-content">
+          <p>${trainObject.origin} &#10140; ${trainObject.dest}</p>
+          <details class="sub-details">
+            <summary>
+              <span class="summary-title">Information och Service ombord</span>
+              <div class="summary-chevron-up">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+              </div>
+            </summary>
+            <ul class="content-list"></ul>
+            <div class="summary-chevron-down">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+            </div>
+          </details>
+        </div>
+        <div class="summary-chevron-down">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+        </div>
     </details>
     `);
       // push all notes in to the content-list
@@ -46,6 +46,7 @@ if (window.matchMedia("(max-width: 600px)").matches) {
     });
   };
 } else {
+  // ending if(mobile) starting else (desktop version)
   $("main").html(`
     <table class="traffic-table table table-striped mt-3">
       <thead class="table-light">
@@ -93,16 +94,17 @@ if (window.matchMedia("(max-width: 600px)").matches) {
   };
 } //end mobile-else
 
-$(".city-input-field").on("keydown", (event) => {
-  if (event.key == "Enter") {
-    if ($("#originField")[0].value == "") return $("#originField").focus();
-    if ($("#destinationField")[0].value == "")
-      return $("#destinationField").focus();
-    searchTrain(
-      $("#originField")[0].value, // Origin cityname
-      $("#destinationField")[0].value, // Destination cityname
-      renderTrainTable //callback
-    );
-    console.log("searched");
-  }
+$(".search-fields").on("submit", (event) => {
+  // if (event.key == "Enter") {
+  if ($("#originField")[0].value == "") return $("#originField").focus();
+  if ($("#destinationField")[0].value == "")
+    return $("#destinationField").focus();
+  searchTrain(
+    $("#originField")[0].value, // Origin cityname
+    $("#destinationField")[0].value, // Destination cityname
+    renderTrainTable //callback
+  );
+  console.log("searched");
+  // }
+  event.preventDefault();
 });
