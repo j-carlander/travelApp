a2ea896da4998e68c45fe35e8d39f289815b0503
j-carlander <johannes.carlander@gmail.com>
2023-01-13 10:01:44 +0100

    added the HTML element template width details for listing the results in mobile view

--- app.js
+++ app.js
@@ -3,6 +3,48 @@ let renderTrainTable;
 if (window.matchMedia("(max-width: 600px)").matches) {
   // The viewport is less than 440 pixels wide
   console.log("This is a mobile device.");
+
+  renderTrainTable = function (data) {
+    data.forEach((trainObject) => {
+      let element = $(`
+      <details class="main-details">
+      <summary>
+      <div class="summary-title">
+        <h3>${trainObject.departTime} &#10140; ${trainObject.arrTime}</h3>
+        <p>${trainObject.name}</p>
+      </div>
+      <div class="summary-chevron-up">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+      </div>
+      </summary>
+      <div class="summary-content">
+        <p>${trainObject.origin} &#10140; ${trainObject.dest}</p>
+        <details class="sub-details">
+          <summary>
+            <span class="summary-title">Information och Service ombord</span>
+            <div class="summary-chevron-up">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+            </div>
+          </summary>
+          <ul class="content-list"></ul>
+          <div class="summary-chevron-down">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+          </div>
+        </details>
+      </div>
+      <div class="summary-chevron-down">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+      </div>
+    </details>
+    `);
+      // push all notes in to the content-list
+      trainObject.notes.Note.forEach((note) => {
+        element.find(".content-list").append(`<li>${note.value}</li>`);
+      });
+
+      $("main").append(element);
+    });
+  };
 } else {
   $("main").html(`
     <table class="traffic-table table table-striped mt-3">
